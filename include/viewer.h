3e975b532c011d709e91ffdbd39fccf9bfeb7cf8
Subramanian Krishnan <subramanian.krishnan@vecnarobotics.com>
2021-04-16 16:24:34 -0400

    threaded viewer tested

--- include/viewer.h
+++ include/viewer.h
@@ -7,10 +7,27 @@ Class definition for the viewer. Would display the tree, obstacles, start and go
 #pragma once
 
 #include <iostream>
-#include <opencv2/opencv.hpp>
-#include <env.h>
+#include <mutex>
 
-class viewer()
+#include <opencv2/opencv.hpp>
+
+class viewer
 {
-    private:
-}
+private:
+    cv::Mat image;
+    int refresh_rate;       // viewer refresh rate in ms
+    
+    bool closeViewer;       // can be fired from main to stop viewer
+    std::mutex closeViewerMutex;    // safeguards thread shared variable closeViewer
+
+public:
+    // ctor for viewer
+    viewer(const cv::Mat& im);
+
+    // main function to display env - Bind to another thread
+    void run();
+
+    // utils to get, set shared vars
+    bool requestViewerClosure();
+    bool isViewerClosed();
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,39 @@
+/*
+Main thread for running the environment, planner and the viewer system
+
+Authors: Subramanian Krishnan, Zhaoyuan Huo, Vaibhav Shete
+*/
+
 #include <iostream>
+#include <thread>
+#include <string>
+#include <unistd.h>
+
+#include "viewer.h"
 
 using namespace std;
 
 int main(){
-    cout << "Testing cmake.." << endl;
-    cout << "Program runs" << endl;
+    cout << "Starting program" << endl;
 
-    return 1;
+    string image_path = "../lavalle.jpg";
+    cv::Mat planning_image = cv::imread(image_path);
+
+    viewer* env_viewer = new viewer(planning_image);
+    std::thread render_thread(&viewer::run, env_viewer);
+
+    // Wait 10 seconds and close viewer
+    for(int i=0; i<10; ++i){
+        cout << "tic tic " << i << endl;
+        sleep(1);
+    }
+
+    // send close signal
+    while(!env_viewer->isViewerClosed())
+        env_viewer->requestViewerClosure();
+
+    // join viewer to main thread
+    render_thread.join();
+
+    return 0;
 }
