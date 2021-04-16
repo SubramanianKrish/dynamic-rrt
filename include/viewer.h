a3baa019303ff6e51d17eaf7c6ded0dfe8447d80
Subramanian Krishnan <subramanian.krishnan@vecnarobotics.com>
2021-04-16 14:57:52 -0400

    updated readme and build instr

--- include/viewer.h
+++ include/viewer.h
@@ -5,3 +5,12 @@ Class definition for the viewer. Would display the tree, obstacles, start and go
 */
 
 #pragma once
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <env.h>
+
+class viewer()
+{
+    private:
+}
