ba247e40bae39ce0f1bb5046de8ecda1f664eef0
Vaibhav1097 <74307578+Vaibhav1097@users.noreply.github.com>
2021-04-23 10:25:32 -0400

    Add files via upload

---
+++ src/testMap.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+#include "Obstacle.h"
+#include "Map.h"
+
+using namespace std;
+
+int main() {
+    Map test_map;
+    // get static obstacle vector in the map
+    vector<Obstacle> stat_obs = test_map.get_static_obs_vec();
+    // get map size
+    double x_size = test_map.get_x_size();
+    double y_size = test_map.get_y_size();
+
+    cout << "Successfully built a test map with " <<  stat_obs.size() << " obstacles" << endl;
+    cout << "The map has a size of (" << x_size << "x" << y_size << ")" << endl;
+
+    // extract the first obstacle
+    Obstacle obs1 = stat_obs[0];
+    vector<double> feat1 = obs1.get_obs_feature(); 
+    // check if obstacle is static
+    bool is_static = obs1.isStatic();
+    if (is_static){
+        cout << "The first obstacle is static"<< endl;
+    }else{
+        cout << "The first obstacle is not static"<< endl;
+    }
+    // get position of the obstacle
+    double x1 = feat1[0];
+    double y1 = feat1[1];
+    if (feat1.size()==3){ // circular obstacle ==> 3 features
+        double radius = feat1[2];
+        cout << "the circular obstacle at position (" << x1 << ", " << y1 << ") has radius " << radius << endl;
+    }else{ // rectangular obstacle ==> 5 features
+        double length = feat1[2];
+        double width = feat1[3];
+        double theta = feat1[4]; // for testing static obstacles, theta should be 0
+        cout << "the rectangular obstacle at position (" << x1 << ", " << y1 << ") has length and width (" << length << ", " << width << ")" << endl;
+    }
+
+    return 0;
+}
