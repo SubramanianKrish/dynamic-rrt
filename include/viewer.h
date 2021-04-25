fe249243d4098cf3bc1627e03702e06086fa717e
Subramanian Krishnan <subramanian.krishnan@vecnarobotics.com>
2021-04-25 16:00:50 -0400

    rebased dyn obst into viewer. Need to write dynObst drawer util

--- include/viewer.h
+++ include/viewer.h
@@ -20,6 +20,7 @@ private:
     cv::Mat background;
     int refresh_rate;               // viewer refresh rate in ms
     Map* map;
+    double map_width, map_height;
     const float scale_factor = 10.0;
     
     bool closeViewer;               // can be fired from main to stop viewer
@@ -31,6 +32,9 @@ public:
 
     // main function to display env - Bind to another thread
     void run();
+
+    // utility drawing functions
+    void DrawObstacle(Obstacle* obst, cv::Mat& scene);
 
     // utils to get, set shared vars
     bool requestViewerClosure();
--- src/viewer.cpp
+++ src/viewer.cpp
@@ -3,48 +3,55 @@
 using namespace std;
 
 viewer::viewer(Map* ptrMap): map(ptrMap), closeViewer(false), refresh_rate(25) // refresh at 40Hz
-{}
+{
+    map_width = map->get_x_size();
+    map_height = map->get_y_size();
+}
+
+void viewer::DrawObstacle(Obstacle* obst, cv::Mat& scene){
+    vector<double> features = obst->get_obs_feature();
+    if(features.size() == 3)
+        circle(scene, cv::Point2f((features[0]*scale_factor), (map_height - features[1])*scale_factor), (int)features[2]*scale_factor, cv::Scalar(0,0,0),CV_FILLED);
+    
+    else if (features.size() == 5)
+    {
+        cv::Point2f rec_center = cv::Point2f(features[0]*scale_factor, (map_height - features[1])*scale_factor);
+        cv::Size2f rec_size = cv::Point2f(features[2]*scale_factor, features[3]*scale_factor); // check len,wid
+        cv::RotatedRect cur_rect = cv::RotatedRect(rec_center, rec_size, -features[4]);
+        
+        cv::Point2f vertices2f[4];
+        cur_rect.points(vertices2f);
+
+        vector<cv::Point> vertices;
+        for(int i=0; i<4; ++i) vertices.push_back(vertices2f[i]);
+        cv::fillConvexPoly(background, vertices, cv::Scalar(0,0,0));
+    }
+
+    else
+        cout << "ERROR: Something is wrong. Obst has "<< features.size() << " features" << endl;
+}
 
 void viewer::run()
 {   
     cout << "Starting viewer .. " << endl;
 
     // Set up white background with map size
-    int width = map->get_x_size(), height = map->get_y_size();
-    background = cv::Mat((int)height*scale_factor, (int)width*scale_factor, CV_8UC3, cv::Scalar(255, 255, 255)); // white bg
+    background = cv::Mat((int)map_height*scale_factor, (int)map_width*scale_factor, CV_8UC3, cv::Scalar(255, 255, 255)); // white bg
     
-    // Draw static obstacles onto background
     vector<Obstacle*> static_obs = map->get_static_obs_vec();
-    for(Obstacle* cur_obst: static_obs){
-        if(cur_obst->isStatic()){
-            vector<double> features = cur_obst->get_obs_feature();
-
+    // Draw static obstacles onto background
+    for(Obstacle* cur_obst: static_obs)
+        if(cur_obst->isStatic())
             // draw circular static obstacles
-            if(cur_obst->isCircle()){
-                if(features.size() != 3) cout << "ERROR: Something is wrong. Circle obst has "<< features.size() << " features" << endl;
-                circle(background, cv::Point2f((features[0]*scale_factor), (height - features[1])*scale_factor), (int)features[2]*scale_factor, cv::Scalar(0,0,0),CV_FILLED);
-            }
+            DrawObstacle(cur_obst, background);
 
-            // draw rectangular obstacles using fill convex poly
-            else{
-                if(features.size() != 5) cout << "ERROR: Something is wrong. Rect obst has "<< features.size() << " features" << endl;
-                cv::Scalar color = cv::Scalar(0,0,0);
-                cv::Point2f rec_center = cv::Point2f(features[0]*scale_factor, (height - features[1])*scale_factor);
-                cv::Size2f rec_size = cv::Point2f(features[2]*scale_factor, features[3]*scale_factor); // check len,wid
-                cv::RotatedRect cur_rect = cv::RotatedRect(rec_center, rec_size, -features[4]);
-                
-                cv::Point2f vertices2f[4];
-                cur_rect.points(vertices2f);
-
-                vector<cv::Point> vertices;
-                for(int i=0; i<4; ++i) vertices.push_back(vertices2f[i]);
-                cv::fillConvexPoly(background, vertices, cv::Scalar(0,0,0));
-            }
-        }
-    }
+    vector<DynamObstacle*> dynamic_obs = map->get_dynam_obs_vec();
 
     while(!isViewerClosed()){
-
+        cv::Mat cur_scene = background.clone();
+        for(auto& cur_dyn_obst: dynamic_obs)
+            // Draw the dynamic obstacle
+            continue;
 
         cv::imshow("Environment display", background);
         cv::waitKey(refresh_rate);
